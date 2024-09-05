aa62f55f737f47cf8376dcfe270ec8f0b9426f85
tasneem94 <tasneemsakif31@gmail.com>
2024-09-05 22:44:16 +0600

    edited styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,11 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useGlobalContext } from "../hooks/useGlobalContext";
 import { DayNightBtn } from "./DayNightBtn";
 import { DropDown } from "./DropDown";
 import { ScrollToTopBtn } from "./ScrollToTopBtn";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { FaSearch } from "react-icons/fa";
-import { FaUserCircle } from "react-icons/fa";
+import { FaSearch, FaBars, FaTimes, FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   const { user } = useAuthContext();
@@ -19,6 +18,7 @@ const Navbar = () => {
     dropdownRef,
     handleClickOutside,
   } = useGlobalContext();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
@@ -28,7 +28,17 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className=" flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0 ">
+    <nav className=" flex justify-between items-center py-8 container mx-auto flex-col md:flex-row gap-5 lg:gap-0 ">
+      {/* Hamburger Icon */}
+      <div className="absolute top-8 left-4 lg:hidden">
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-2xl text-gray-900 dark:text-gray-200"
+        >
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+
       <div className="text-2xl lg:text-3xl text-green-700 dark:text-sky-600 font-semibold tracking-wider">
         <NavLink to={"/"} className="cursor-pointer">
           FOOD RECIPE
@@ -41,10 +51,18 @@ const Navbar = () => {
           value={searchParam}
           onChange={(e) => setSearchParam(e.target.value)}
           placeholder="Search Recipe..."
-          className="bg-white/75 dark:bg-gray-700 px-8 py-3 rounded-full outline-none lg:w-96 shadow-lg shadow-gray-200 dark:shadow-gray-800 focus:shadow-gray-300 dark:focus:shadow-gray-900 pr-10" // Add pr-10 to provide space for the icon
+          className="bg-white/75 dark:bg-gray-700 px-11 py-3 rounded-full outline-none lg:w-96 shadow-lg shadow-gray-200 dark:shadow-gray-800 focus:shadow-gray-300 dark:focus:shadow-gray-900 pr-10" // Add pr-10 to provide space for the icon
         />
+        {searchParam !== "" && (
+          <FaTimes
+            className="absolute right-5 cursor-pointer text-gray-500 dark:text-gray-300"
+            onClick={() => {
+              setSearchParam("");
+            }}
+          />
+        )}
         <FaSearch
-          className="absolute right-5 cursor-pointer text-gray-500 dark:text-gray-300"
+          className="absolute left-5 cursor-pointer text-gray-500 dark:text-gray-300"
           onClick={handleSubmit}
         />
       </form>
@@ -65,30 +83,27 @@ const Navbar = () => {
             Favorites
           </NavLink>
         </li>
-
-        {!user && (
-          <li>
-            <NavLink
-              to={"/login"}
-              className="text-black hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-100 cursor-pointer"
-            >
-              Login
-            </NavLink>
-          </li>
-        )}
-        {user && (
-          <li className="relative" ref={dropdownRef}>
+      </ul>
+      <div className="fixed flex items-center gap-4 top-8 right-4 lg:top-11 z-50">
+        {user ? (
+          <div className="relative" ref={dropdownRef}>
             <button
               onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              className="flex items-center space-x-2 text-black hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-200 cursor-pointer"
+              className="flex items-center space-x-2 text-gray-800 hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-200 cursor-pointer"
             >
               <FaUserCircle className="text-2xl" />
             </button>
+
             <DropDown />
-          </li>
+          </div>
+        ) : (
+          <NavLink
+            to={"/login"}
+            className="text-gray-800 hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-100 cursor-pointer"
+          >
+            Login
+          </NavLink>
         )}
-      </ul>
-      <div className="fixed top-8 right-4 lg:top-11 z-50">
         <DayNightBtn />
       </div>
       <ScrollToTopBtn />
