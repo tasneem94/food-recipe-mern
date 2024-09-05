467596e64367fddc184565f205c334ae68a74f3d
tasneem94 <tasneemsakif31@gmail.com>
2024-09-06 02:48:11 +0600

    styling added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -30,9 +30,9 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className=" flex justify-between items-center py-8 container mx-auto flex-col md:flex-row gap-5 lg:gap-0 ">
+    <nav className=" flex flex-row justify-between items-center px-0 py-8 container mx-auto gap-5">
       {/* Hamburger Icon */}
-      <div ref={menuRef} className="absolute top-8 left-4 lg:hidden">
+      <div ref={menuRef} className="lg:hidden">
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
           className="text-2xl text-gray-900 dark:text-gray-200"
@@ -46,13 +46,14 @@ const Navbar = () => {
           FOOD RECIPE
         </NavLink>
       </div>
-      <form onSubmit={handleSubmit} className="relative flex items-center">
+
+      <form onSubmit={handleSubmit} className="relative flex items-center ">
         <input
           type="text"
           name="search"
           value={searchParam}
           onChange={(e) => setSearchParam(e.target.value)}
-          placeholder="Search Recipe..."
+          placeholder="Search..."
           className="bg-white/75 dark:bg-gray-700 px-11 py-3 rounded-full outline-none lg:w-96 shadow-lg shadow-gray-200 dark:shadow-gray-800 focus:shadow-gray-300 dark:focus:shadow-gray-900 pr-10" // Add pr-10 to provide space for the icon
         />
         {searchParam !== "" && (
@@ -68,6 +69,7 @@ const Navbar = () => {
           onClick={handleSubmit}
         />
       </form>
+
       <ul className="flex gap-5 lg:mr-3">
         <li>
           <NavLink
@@ -85,29 +87,33 @@ const Navbar = () => {
             Favorites
           </NavLink>
         </li>
-      </ul>
-      <div className="fixed flex items-center gap-4 top-8 right-4 lg:top-11 z-50">
-        {user ? (
-          <div className="relative" ref={dropdownRef}>
-            <button
-              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              className="flex items-center space-x-2 text-gray-800 hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-200 cursor-pointer"
-            >
-              <FaUserCircle className="text-2xl" />
-            </button>
 
-            <DropDown />
-          </div>
-        ) : (
-          <NavLink
-            to={"/login"}
-            className="text-gray-800 hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-100 cursor-pointer"
-          >
-            Login
-          </NavLink>
-        )}
-        <DayNightBtn />
-      </div>
+        <li>
+          {user ? (
+            <div className="relative" ref={dropdownRef}>
+              <button
+                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                className="flex items-center space-x-2 text-gray-800 hover:text-gray-700 dark:text-white/90 dark:hover:text-white/80 duration-200 cursor-pointer"
+              >
+                <FaUserCircle className="text-2xl" />
+              </button>
+
+              <DropDown />
+            </div>
+          ) : (
+            <NavLink
+              to={"/login"}
+              className="bg-green-500/30 dark:bg-sky-500/40 px-2 py-1 rounded-md text-gray-800 hover:text-gray-600 dark:text-white/90 dark:hover:text-white/80 duration-100 cursor-pointer"
+            >
+              Login
+            </NavLink>
+          )}
+        </li>
+        <li>
+          <DayNightBtn />
+        </li>
+      </ul>
+
       <ScrollToTopBtn />
     </nav>
   );
