8fdd70d1991505aa994bb5c2035b97f4e4df76c8
tasneem94 <tasneemsakif31@gmail.com>
2024-09-10 20:08:51 +0600

    updated backend route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const favoritesRoutes = require("./routes/favorites");
 const userRoutes = require("./routes/user.js");
 const cors = require("cors");
 
